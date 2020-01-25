ec00f8f367e587a7dc8ba290747d83a5e35a4596
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-25 12:45:04 +0200

    action creators for getting, updating and deleting streams added

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,6 +16,27 @@ export const signOut = () => {
 
 export const createStream = formValues => {
   return async dispatch => {
-    streams.post("/streams", formValues);
+    const response = await streams.post("/streams", formValues);
+    dispatch({ type: types.CREATE_STREAM, payload: response.data });
   };
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: types.FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: types.FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: types.EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: types.DELETE_STREAM });
 };
