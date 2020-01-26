75d0475f75e8c2586d3d5cb0051bbaa0457edd65
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-26 21:18:01 +0200

    adding create stream button and conditionally rendering it if user is signed in

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin = stream => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  };
+
   renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -19,11 +33,24 @@ class StreamList extends React.Component {
       );
     });
   };
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link className="ui button primary" to="/streams/new">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -31,6 +58,8 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
   return {
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
     streams: Object.values(state.streams)
   };
 };
