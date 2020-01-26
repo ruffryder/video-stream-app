3683ed3f638dac4abf9b984a490c4a23541e75d2
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-27 00:18:05 +0200

    implementing edit and create flow. creating a new StreamForm Component

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -1,62 +1,20 @@
 import React, { Component } from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends Component {
-  renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
-  renderInput = ({ input, label, meta }) => {
-    return (
-      <div className="field">
-        <label>{label}</label>
-        <input {...input} />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field label="Enter Title" name="title" component={this.renderInput} />
-        <Field
-          label="Enter Description"
-          name="description"
-          component={this.renderInput}
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = "Please, enter a title";
-  }
-  if (!formValues.description) {
-    errors.description = "Please, enter a description";
-  }
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate",
-  validate
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
