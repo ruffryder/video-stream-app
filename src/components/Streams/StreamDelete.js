324872c80e935cd0748f146d740b59ced364fb6e
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-27 12:18:24 +0200

    creating a new Modal component and using it inside StreamDelete.

--- src/components/Streams/StreamDelete.js
+++ src/components/Streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
 
 function StreamDelete() {
-  return <div>Stream Delete</div>;
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+  return (
+    <div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </div>
+  );
 }
 
 export default StreamDelete;
