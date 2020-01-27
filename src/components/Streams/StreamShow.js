9ebb53ab480cd0e7015b5387b02ac2a9c25c6246
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-01-27 15:43:39 +0200

    adding live streaming functionality to project

--- src/components/Streams/StreamShow.js
+++ src/components/Streams/StreamShow.js
@@ -1,17 +1,42 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  };
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
     return (
       <div>
+        <video controls ref={this.videoRef} style={{ width: "100%" }} />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
